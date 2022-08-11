972c1b98366e3e13331a84d342f32b194f9fed2e
Yejin Ha <101123653+beAedin@users.noreply.github.com>
2022-08-11 23:23:11 +0900

    ❤ Completed Esperanto flash card

--- flash-card-project/main.py
+++ flash-card-project/main.py
@@ -4,21 +4,61 @@ import random
 
 
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+word_dict = {}
 
+########### READ CSV ###########
 
-########### CSV ###########
-
-data = pandas.read_csv("data/esperanto.csv")
-# match eo-en
-word_dict = data.to_dict(orient="records")
+try:
+    data = pandas.read_csv("data/esperanto.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/esperanto.csv")
+    word_dict = original_data.to_dict(orient="records")
+else:
+    word_dict = data.to_dict(orient="records")
 
 ########## CARD ###########
 
+
 def next_card():
+    # appear esperanto_word
+    # recall flip_card
+
+
+    global current_card, flip_timer
+
+    # Reset flip_timer
+    window.after_cancel(flip_timer)
+
+    # randomly choose from word list
     current_card = random.choice(word_dict)
-    print(current_card["Esperanto"])
-    canvas.itemconfig(text_eo, text="Esperanto")
-    canvas.itemconfig(text_en, text=current_card["Esperanto"])
+
+    # draw on canvas [esperanto_word]
+    canvas.itemconfig(title, text="Esperanto", fill="black")
+    canvas.itemconfig(word, text=current_card["Esperanto"], fill="black")
+
+    # Recall flip_timer
+    window.after(3000, func=flip_card)
+
+
+
+########## 3000ms->flip ##########
+def flip_card():
+    canvas.itemconfig(title, text="English", fill="green")
+    canvas.itemconfig(word, text=current_card["English"], fill="green")
+    canvas.itemconfig(card_background, image=card_back)
+
+
+########## Correct ###########
+def know_word():
+    # Remove cur_card from cur_data
+    word_dict.remove(current_card)
+
+    # Make a dataframe -> csv
+    data = pandas.DataFrame(word_dict)
+    data.to_csv("data/memorize_word_list", index=False)
+
+    next_card()
 
 
 ########### UI ############
@@ -26,15 +66,20 @@ window = Tk();
 window.title("Flash Card")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
 # Canvas
 canvas = Canvas(width=800, height=526)
-# card_back = PhotoImage(file="images/card_back.png")
+card_back = PhotoImage(file="images/card_back.png")
 card_front = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front)
+
+card_background = PhotoImage(file="images/card_front.png")
+
+canvas.create_image(400, 263, image=card_background)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 
-text_eo = canvas.create_text(400, 150, text="Esperanto", font=("Ariel", 40, "italic"))
-text_en = canvas.create_text(400, 263, text="English", font=("Ariel", 60, "bold"))
+title = canvas.create_text(400, 150, text="Esperanto", font=("Ariel", 40, "italic"))
+word = canvas.create_text(400, 263, text="English", font=("Ariel", 60, "bold"))
 canvas.grid(column=0,row=0, columnspan=2)
 
 #Button
@@ -42,7 +87,7 @@ img_btn_right = PhotoImage(file="images/right.png")
 img_btn_wrong = PhotoImage(file="images/wrong.png")
 
 
-btn_right = Button(image=img_btn_right, highlightthickness=0, command=next_card)
+btn_right = Button(image=img_btn_right, highlightthickness=0, command=know_word)
 btn_right.grid(column=0, row=1)
 
 btn_wrong = Button(image=img_btn_wrong, highlightthickness=0, command=next_card)
