a120663dd64f064e0ec408fb5007064c451fe281
Yejin Ha <101123653+beAedin@users.noreply.github.com>
2022-08-21 00:39:45 +0900

    💛 twilio api, .env file

--- rain_alert/main.py
+++ rain_alert/main.py
@@ -1,6 +1,13 @@
 import requests
+from twilio.rest import Client
+from dotenv import load_dotenv
+import os
 
-OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+load_dotenv()
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/weather"
+account_sid = os.environ.get('twilio_account_sid')
+auth_token = os.environ.get('twilio_auth_token')
 
 api_key = "d64e424ef104c4eb25b52a4586b41ad5"
 
@@ -11,4 +18,14 @@ weather_params = {
 }
 
 response = requests.get(OWM_Endpoint, params=weather_params)
-print(response.status_code)
+print(response.json())
+
+client = Client(account_sid, auth_token)
+message = client.messages \
+                .create(
+    body="자니?",
+    from_ = "+14249552394",
+    to = "+8201065237536"
+)
+
+print(message.status)
