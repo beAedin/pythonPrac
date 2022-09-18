efccd5ebdb7bda410be7a7878231700849f35687
Yejin Ha <101123653+beAedin@users.noreply.github.com>
2022-09-18 23:49:44 +0900

    😎 Decorator

--- flask_prac/hello.py
+++ flask_prac/hello.py
@@ -1,10 +1,19 @@
 from flask import Flask
 app = Flask(__name__)
 
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+    return wrapper
+
 @app.route('/')
+@make_bold
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/username/<name>/<int:year>')
+def greet(name, year):
+    return f"Hello {name} You are {year}"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
